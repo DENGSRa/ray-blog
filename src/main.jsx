2a39a2576b386ac985cc4406c275f5398080e02b
shengra <112135588+DENGSRa@users.noreply.github.com>
2026-07-23 23:57:12 +0800

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
+import './styles.css';
+import './styles/interactions.css';
+import './styles/custom.css';
+
+createRoot(document.getElementById('root')).render(<App />);
